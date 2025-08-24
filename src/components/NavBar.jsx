082fb1b6d8488e0bf84c6beed23c1cf745bbe817
Rida Irfan <ridairfan264@gmail.com>
2025-08-24 13:16:39 +0500

    NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
       <nav>
         <ul className={`list-none flex items-start text-[16px] gap-[5px] ${className || ""}`}>
           {items.map((item, index) => (
-            <li key={index} className="flex items-center cursor-pointer">
+            <li key={index} className="flex justify-center items-center cursor-pointer">
               <span>{item}</span>
               {item === "Shop" && (
                 <svg
@@ -64,7 +64,7 @@ function NavBar() {
       </div>
 
       {/* --- Header --- */}
-      <header className="py-[15px] px-[20px]">
+      <header className="md:py-[20px} md:px-[100px] py-[15px] px-[20px]">
         <div className="flex justify-between items-center h-[48px] gap-[40px]">
           {/* Left section (logo + menu toggle) */}
           <div className="flex items-center gap-[20px]">
@@ -101,33 +101,36 @@ function NavBar() {
               )}
             </button>
 
-            <div className="font-bold font-integral text-[25px] translate-y-[-5px]">
+            <div className="md:text-[32px] font-bold font-integral text-[25px] translate-y-[-5px]">
               SHOP.CO
             </div>
           </div>
 
           {/* Center nav (desktop only) */}
           <div className="hidden md:block">
-            <NavMenu items={navItems} className="flex flex-row gap-[20px]" />
+            <NavMenu items={navItems} className=" justify-center items-center font-satoshi text-[16px] flex flex-row gap-[20px]" />
           </div>
 
           {/* Search bar (desktop only) */}
-          <div className="hidden md:flex md:h-[100%] md:flex-1 md:bg-[#f0f0f0] md:rounded-[30px] items-center md:px-[5px] md:py-[10px] md:m-[10px]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.34-4.34" />
-            </svg>
+          <div className="hidden md:flex md:h-full md:flex-1 md:bg-[#f0f0f0] md:rounded-[30px] items-center md:px-[10px] md:py-[5px] md:m-[10px]">
+             <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-search-icon lucide-search"
+          >
+            <path d="m21 21-4.34-4.34" />
+            <circle cx="11" cy="11" r="8" />
+          </svg>
+          
             <input
-              className="md:w-[100%] md:ml-[5px] md:font-satoshi ml-2 focus:outline-none"
+              className="md:w-full md:ml-[5px] md:font-satoshi focus:outline-none"
               type="text"
               placeholder="Search for products..."
             />
