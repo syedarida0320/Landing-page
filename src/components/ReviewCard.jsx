193dd741254a094274373632b5260830ac9584c5
Rida Irfan <ridairfan264@gmail.com>
2025-08-25 19:50:36 +0500

    Update ReviewCard.jsx

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,15 +2,15 @@ const ReviewCard = ({ rating, name, verified, text, isActive }) => {
   if (!isActive) return null; 
 
   return (
-    <div className="review-card bg-[#f7f7f7] flex flex-col justify-start rounded-[20px] flex-1 p-[24px] shadow-[0_4px_12px_rgba(0,0,0,0.05)] border border-[#e4dcdc]">
-      <div className="customer-rating mb-[12px] text-[28px] text-[#f5b400]">
-        {rating}
+    <div className=" md:rounded-[16px] md:py-[28px] md:px-[32px]  bg-[#f7f7f7]  flex flex-col justify-start rounded-[20px] flex-1 p-[24px] shadow-[0_4px_12px_rgba(0,0,0,0.05)] border border-[#e4dcdc] review-card">
+      <div className="customer-rating md:text-[18px] mb-[12px] text-[28px] text-[#f5b400]">
+        {"★".repeat(rating)}{"☆".repeat(5 - rating)}
       </div>
-      <div className="name mb-[10px] flex items-center gap-[6px] text-[16px] font-satoshi-bold leading-[22px] tracking-normal">
+      <div className="name md:text-[20px] mb-[10px] flex items-center gap-[6px] text-[16px] font-satoshi-bold leading-[22px] tracking-normal">
         {name}
         {verified && <span className="verified text-[14px] text-green-600">✔️</span>}
       </div>
-      <p className="review-text m-0 text-[14px] leading-[20px] font-satoshi font-normal text-[#555]">
+      <p className="review-text md:text-[16px] md:leading-[22px] m-0 text-[14px] leading-[20px] font-satoshi font-normal text-[#555]">
         {text}
       </p>
     </div>
